e96f4b07169cde4e90bfdda01b9136a2e93e24cf
Noriaki Sakai <noriaki.sakai.develop@gmail.com>
2015-12-07 22:20:37 +0900

    Initial Commit

---
+++ Piano/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Piano
+//
+//  Created by 酒井紀明 on 2015/12/07.
+//  Copyright © 2015年 noriaki.sakai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Piano/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Piano
+//
+//  Created by 酒井紀明 on 2015/12/07.
+//  Copyright © 2015年 noriaki.sakai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
